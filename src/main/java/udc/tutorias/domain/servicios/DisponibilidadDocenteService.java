8f7798abae707963f28d18a8eb90f9b687eb21b1
ale18-ship <acerap@unicartagena.edu.co>
2025-09-30 17:02:44 -0500

    feat(domain): agregar servicios de dominio (DisponibilidadDocenteService, ValidacionHorarioService, NotificacionService)

---
+++ src/main/java/udc/tutorias/domain/servicios/DisponibilidadDocenteService.java
@@ -0,0 +1,12 @@
+package udc.tutorias.domain.servicios;
+
+import udc.tutorias.domain.model.Docente;
+import udc.tutorias.domain.model.vo.FechaHora;
+
+public class DisponibilidadDocenteService {
+    public boolean estaDisponible(Docente docente, FechaHora fechaHora) {
+
+        return true;
+    }
+
+}
---
+++ src/main/java/udc/tutorias/domain/servicios/ValidacionHorarioService.java
@@ -0,0 +1,10 @@
+package udc.tutorias.domain.servicios;
+
+import udc.tutorias.domain.model.vo.FechaHora;
+
+public class ValidacionHorarioService {
+    public boolean esValido(FechaHora fechaHora) {
+
+        return true;
+    }
+}
